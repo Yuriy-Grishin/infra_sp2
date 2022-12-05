9b2175da1a70a074e56a8cef047b661f6a18b60b
Юрий Гришин <grishinyuriy@mail.ru>
2022-12-06 01:55:48 +0300

    3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,11 +1,15 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -63,17 +67,6 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 # Database
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', default='db_name'),
-#         'USER': os.getenv('POSTGRES_USER', default='postgres_user'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres_password'),
-#         'HOST': os.getenv('DB_HOST', default='db'),
-#         'PORT': os.getenv('DB_PORT', default='5432')
-#     }
-# }
-
 if DEBUG:
 
     DATABASES = {
@@ -94,16 +87,6 @@ else:
         }
     }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-#         "NAME": os.getenv("DB_NAME", default="postgres"),
-#         "USER": os.getenv("POSTGRES_USER", default="postgres"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-#         "HOST": os.getenv("DB_HOST", default="postgres"),
-#         "PORT": os.getenv("DB_PORT", default=5432),
-#     }
-# }
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
